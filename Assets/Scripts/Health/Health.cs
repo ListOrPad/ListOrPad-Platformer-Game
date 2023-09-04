611df07a3f5abc56515de6f1cf986a8b2b357189
ListOrPad <ListOrPad@gmail.com>
2023-09-04 17:58:43 +0300

    v 0.0.7
    *added enemy and player layers in unity
    *added flashing player effect on player get hurt

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("health")]
     [SerializeField] private float startingHealth;
     public float CurrentHealth { get; private set; }
     private Animator animator;
     private bool dead;
+    [Header("IFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         CurrentHealth = startingHealth;
         animator = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
@@ -22,6 +28,7 @@ public class Health : MonoBehaviour
         if(CurrentHealth > 0)
         {
             animator.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -36,5 +43,17 @@ public class Health : MonoBehaviour
     public void AddHealth(float value)
     {
         CurrentHealth = Mathf.Clamp(CurrentHealth + value, 0, startingHealth);
+    }
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(9, 10, true);
+        for (int i = 0;i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(9, 10, false);
     }
 }
